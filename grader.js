32c8f7e51722c5e8589800cc5f105dea10830dc3
Tom Nielsen <hr.tomnielsen@gmail.com>
2013-07-21 10:15:59 +0200

    formatting changes

--- grader.js
+++ grader.js
@@ -46,15 +46,15 @@ var loadChecks = function(checksfile) {
 };
 
 var checkHtmlFile = function(htmlfile, checksfile, URLcontent) {
-	if (URLcontent) {
-		$ = cheerio.load(URLcontent);
-	} else {
-    	$ = cheerioHtmlFile(htmlfile);
-	}
+    if (URLcontent) {
+        $ = cheerio.load(URLcontent);
+    } else {
+        $ = cheerioHtmlFile(htmlfile);
+    }
     var checks = loadChecks(checksfile).sort();
     var out    = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
+        var present     = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
