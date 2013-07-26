3b8cf1291bb65eb6ce57b0837399e0202d01f773
Tom Nielsen <hr.tomnielsen@gmail.com>
2013-07-26 11:30:07 +0200

    Revert "Updated web.js to reflect the course file"
    This reverts commit 39efe884d14353510ca271e6f4c35017a8f60353.

--- web.js
+++ web.js
@@ -1,15 +1,17 @@
 var express    = require('express');
-var fs         = require('fs');
 
 var app        = express.createServer(express.logger());
-var htmlfile   = "index.html";
+
+var fs         = require('fs');
+var infile     = "index.html";
+var filebuffer = fs.readFileSync(infile);
+var filestr    = filebuffer.toString('utf-8');
 
 app.get('/', function(request, response) {
-    var html = fs.readFileSync(htmlfile).toString();
-    response.send(html);
+  response.send(filestr);
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
-    console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
